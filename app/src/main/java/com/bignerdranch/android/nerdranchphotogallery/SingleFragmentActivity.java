e3b452854ac5deadee7f978424a2b01a32d348c9
Timothy Miller <timothy.miller.cs@gmail.com>
2016-08-17 01:08:36 -0400

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/nerdranchphotogallery/SingleFragmentActivity.java
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.nerdranchphotogallery;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TMiller on 7/28/2016.
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+}
